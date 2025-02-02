ccc8f905db1a270c730cdeaee3256907fb39395e
niiquash <qua16006@byui.edu>
2025-02-01 18:40:54 -0700

    updated projects section to carousel with navigation dots.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ const hamburgerButton = document.getElementById("hamburger");
 const menu = document.querySelector(".header__nav--menu");
 const navItems = document.querySelectorAll(".header__nav--item");
 
+const projectItems = document.querySelectorAll(".main__project");
+const dots = document.querySelectorAll(".carousel__dot");
+const projectContainer = document.querySelector(
+  ".main__projects--container-carousel"
+);
+let currentIndex = 0;
+
 const toggleShow = () => {
   menu.classList.toggle("show");
 };
@@ -13,4 +20,18 @@ const removeShow = () => {
 hamburgerButton.addEventListener("click", toggleShow);
 navItems.forEach((navItem) => {
   navItem.addEventListener("click", removeShow);
+});
+
+const updateCarousel = () => {
+  projectContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("active", index === currentIndex);
+  });
+};
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    currentIndex = index;
+    updateCarousel();
+  });
 });
